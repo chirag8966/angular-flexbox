6f599f26e5b472b54955b0bf3423d96edf288add
chirag8966 <chirag8966@github.com>
2020-05-21 19:07:14 +0530

    component wise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TablesComponent } from './tables/tables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TablesComponent
   ],
   imports: [
     BrowserModule,
